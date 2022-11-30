a58ed16041d285f5b7bb7ced331b03dbb797f410
david <dbour.developer@gmail.com>
2022-11-30 13:29:29 -0800

    fasterapi test

--- fasterapi/main.py
+++ fasterapi/main.py
@@ -8,3 +8,7 @@ config = os.environ.get("PAGE_NAME", "DEFAULT")
 @app.get("/")
 def home():
     return {"secretCode": config}
+
+@app.get("/info")
+def info():
+    return {"info": "some configuration"}
